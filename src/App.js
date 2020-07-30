24a2fcca671fed4cfa2cb28bdd8bf2c3664aad4e
Wasurat Thampanya <wasurat.th@baac.or.th>
2020-07-30 11:08:04 +0700

    add redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+
+import { UserOutlined, CaretDownOutlined } from '@ant-design/icons';
+import { Layout, Menu, Avatar } from 'antd';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Router>
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1"><Link to="/" >Home</Link></Menu.Item>
+            <Menu.Item key="3"><Link to="/page1" >Login</Link></Menu.Item>
+            <Menu.Item key="4"><Link to="/page2" >Sign Up</Link></Menu.Item>
+            <Menu.Item key="5"><Link to="/page3" >My Profile</Link></Menu.Item>
+            <Menu.Item key="6"><Link to="/page4" >Friend Profile</Link></Menu.Item>
+            <Menu.Item key="7"><Link to="/page5" >Friend Profile</Link></Menu.Item>
+            {
+            /*
+            <div style={{ float: 'right', width: 200 }} >
+              <div style={{ float: 'left', marginRight: 10 }} >นาย วสุรัฐ ธรรมปัญญา</div>
+              <Avatar size={48} icon={<UserOutlined />} style={{ float: 'left', top: 10 }} />
+              <CaretDownOutlined size={48} />
+            </div>
+            */
+            }
+
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div className="site-layout-content" style={{ paddingTop: 30 }}>
+            <Switch>
+              <Route path="/page1"></Route>
+              <Route path="/page2"></Route>
+              <Route path="/page3"></Route>
+              <Route path="/page4"></Route>
+              <Route path="/page5"></Route>
+              <Route path="/page6"></Route>
+              <Route path="/page7"></Route>
+              <Route path="/"></Route>
+            </Switch>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}></Footer>
+      </Router>
+    </Layout>
   );
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
- 
+import reducers from './reducers';
+
 // this is combining all the reducers we have in the app
 // you can access each of them using state.exercise0, state.exercise1, etc...
-const rootReducer = combineReducers({ });
+const rootReducer = combineReducers({ 
+    reducers 
+});
 
 export default rootReducer;
